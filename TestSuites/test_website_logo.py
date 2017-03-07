3db647443382c7ebafedbb9bbb5e788f3392f788
Ankur Vaid <vaid.ankur@tftus.com>
2017-03-07 20:36:45 +0530

    First commit for TripAdvisor POM

---
+++ TestSuites/test_website_logo.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from TripAdvisorPages.HomePage import *
+
+
+def test_logo_redirection():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get('https://www.tripadvisor.com')
+    driver.find_element(*logo).click()
+    actual_url = driver.current_url
+    assert 'https://www.tripadvisor.in' in actual_url
---
+++ TripAdvisorPages/HomePage.py
@@ -0,0 +1,4 @@
+from selenium.webdriver.common.by import By
+
+logo = (By.CSS_SELECTOR, "span.topLogo>a.logoWrap>img.svg-taLogo")
+
