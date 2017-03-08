75f94bf80fcbcc3d8e5bca666b9907586fb6c67f
Ankur Vaid <vaid.ankur@tftus.com>
2017-03-08 21:40:01 +0530

    Third Commit ; Added TC for test_valid_hotel_search_dropdown_select in TestSUites and search_dropdown_hotels and search_dropdown_rajasthan in HomePage

--- TestSuites/test_search_functionality.py
+++ TestSuites/test_search_functionality.py
@@ -1,3 +1,4 @@
+from time import sleep
 from selenium import webdriver
 from TripAdvisorPages.HomePage import *
 from TripAdvisorPages.RajasthanHotelsPage import *
@@ -6,8 +7,28 @@ def test_valid_hotel_search_text_input():
     driver = webdriver.Chrome()
     driver.maximize_window()
     driver.get('https://www.tripadvisor.com')
-    driver.find_element(*find_search_box).send_keys("hotel")
+    driver.find_element(*find_search_box).click()
+    driver.find_element(*find_search_box).send_keys("hotels")
+    driver.find_element(*near_search_box).click()
+    sleep(1)
     driver.find_element(*near_search_box).send_keys("rajasthan")
+    sleep(2)
+    driver.find_element(*search_button).click()
+    welcome_text = driver.find_element(*welcome_message).text
+    assert welcome_text == "Rajasthan Hotels"
+
+def test_valid_hotel_search_dropdown_select():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get('https://www.tripadvisor.com')
+    driver.find_element(*find_search_box).click()
+    sleep(2)
+    driver.find_element(*search_dropdown_hotels).click()
+    driver.find_element(*near_search_box).click()
+    sleep(1)
+    driver.find_element(*near_search_box).send_keys("rajasthan")
+    sleep(2)
+    driver.find_element(*search_dropdown_rajasthan).click()
     driver.find_element(*search_button).click()
     welcome_text = driver.find_element(*welcome_message).text
     assert welcome_text == "Rajasthan Hotels"
--- TripAdvisorPages/HomePage.py
+++ TripAdvisorPages/HomePage.py
@@ -3,5 +3,6 @@ from selenium.webdriver.common.by import By
 logo = (By.CSS_SELECTOR, "span.topLogo>a.logoWrap>img.svg-taLogo")
 find_search_box = (By.ID, "mainSearch")
 near_search_box = (By.ID, "GEO_SCOPED_SEARCH_INPUT")
-search_button = (By.CSS_SELECTOR, "button#SEARCH_BUTTON")
-
+search_button = (By.CSS_SELECTOR, "button#SEARCH_BUTTON>div#SEARCH_BUTTON_CONTENT div.inner")
+search_dropdown_hotels = (By.CSS_SELECTOR, "span.poi-name.primaryText.no-geo>span.poi_overview_item")
+search_dropdown_rajasthan = (By.CSS_SELECTOR, "li.displayItem.result.selected span.poi-name.primaryText>b")
